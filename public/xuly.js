87e74adbcc5bfb68a16ebc8df9d5a69e43dead16
khanhlee99 <levietkhanh99@gmail.com>
2021-04-02 11:07:04 +0700

    make it better

---
+++ public/xuly.js
@@ -0,0 +1,57 @@
+var socket = io("http://localhost:3000");
+
+socket.on('server-send-user-tontai', function(){
+    alert("user da ton tai")
+})
+
+socket.on('server-send-user-hople', function(data){
+    $("#currentUser").html(data);
+    $("#formRegister").hide(2000);
+    $("#formMessage").show(1000);
+})
+
+socket.on('server-send-list-user', function(data){
+    $('#userOnline').html("");
+    data.forEach(function(user){ 
+        $('#userOnline').append("<div class='user'>"+user+"</div>");
+    })
+})
+
+socket.on('server-send-all-user', function(data){
+    $('#messages').append("<div>"+data.username + ": " + data.content +"</div>")
+})
+
+socket.on('thong-bao-dang-chat', function(data){
+    $('#thong-bao').html(data + " dang nhap");
+})
+
+socket.on('thong-bao-dang-stop', function(data){
+    $('#thong-bao').html("");
+})
+
+
+$(document).ready(function(){
+    $("#formRegister").show();
+    $("#formMessage").hide();
+
+    $("#btnRegister").click(function(){
+        socket.emit("client-register", $("#username").val());
+    })
+    
+    $('#btnSend').click(function(){
+        socket.emit('client-send-message', $('#messageContent').val())
+    })
+
+    $('#messageContent').focusin(function(){
+        socket.emit('toi-dang-chat');
+    })
+
+    $('#messageContent').focusout(function(){
+        socket.emit('toi-stop-chat');
+    })
+
+    $('#btnLogout').click(function(){
+        socket.emit('client-logout');
+        $("#formMessage").hide();
+    })
+})
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+users = [];
+
+io.on("connection", function(socket){
+    console.log("connected: " + socket.id);
+
+    socket.on("disconnect", function(){
+        console.log("disconnect: " + socket.id);
+    })
+
+    socket.on('client-register', function(data){
+        if(users.indexOf(data) === -1){
+            users.push(data);
+            socket.username = data;
+            socket.emit('server-send-user-hople', data);
+            io.sockets.emit("server-send-list-user", users)
+        }
+        else{
+            socket.emit('server-send-user-tontai');
+        }
+
+        socket.on('client-send-message',function(data){
+            io.sockets.emit('server-send-all-user', {username: socket.username, content: data})
+        })
+
+        socket.on('toi-dang-chat', function(){
+            socket.broadcast.emit('thong-bao-dang-chat', socket.username)
+        })
+
+        socket.on('toi-stop-chat', function(){
+            socket.broadcast.emit('thong-bao-dang-stop', socket.username)
+        })
+
+        socket.on('client-logout', function(){
+            users.splice(users.indexOf(socket.username), 1);
+            socket.broadcast.emit('server-send-list-user', users)
+        })
+
+
+
+        // io.sockets.emit('server-send-data', data + '888');
+        // socket.emit('server-send-data', data + '888');
+        // socket.broadcast.emit('server-send-data', data + '888');
+
+    })
+});
+
+app.get("/", function(req, res){
+    res.render("trangchu");
+});
